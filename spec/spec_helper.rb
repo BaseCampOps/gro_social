0c00469e4624601bc0667d01761a167eaa6b0546
James Thompson <james@buypd.com>
2015-01-15 15:05:25 -0700

    Updated project to report coverage to Code Climate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec/'
+if ENV['CI']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
