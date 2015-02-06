3b4c6715451d39d67f3a5ea1702c2a1f3d3ac880
James Thompson <james@buypd.com>
2015-02-05 22:54:13 -0700

    Implemented basic GroSocial::Users#[] functionality.

--- lib/gro_social/users.rb
+++ lib/gro_social/users.rb
@@ -3,7 +3,9 @@ module GroSocial
     include Enumerable
 
     def self.[](id)
-
+      result = GroSocial::Client.request('Users', :get, {id: id.to_s})
+      return nil if result['result']['user'].nil?
+      GroSocial::User.new(result['result']['user'])
     end
 
     def self.each(&block)
--- spec/gro_social/users_spec.rb
+++ spec/gro_social/users_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe GroSocial::Users do
+  before(:each) do
+    GroSocial::Client.test_mode = true
+    GroSocial::Client.api_key = ENV['GROSOCIAL_KEY']
+    GroSocial::Client.api_password = ENV['GROSOCIAL_PASSWORD']
+  end
+
   describe '.[]' do
     it 'responds' do
       expect(GroSocial::Users).to respond_to(:[])
     end
 
-    it 'returns a GroSocial::User when one exists'
+    it 'returns a GroSocial::User when one exists', :vcr do
+      expect(GroSocial::Users['20183']).to be_a_kind_of(GroSocial::User)
+    end
 
-    it 'returns nil when a User does not exist'
+    it 'returns nil when a User does not exist', :vcr do
+      expect(GroSocial::Users['0']).to eq(nil)
+    end
   end
 
   describe '.each' do
@@ -16,6 +26,10 @@ RSpec.describe GroSocial::Users do
       expect(GroSocial::Users).to respond_to(:each)
     end
 
-    it 'returns GroSocial::User instances'
+    it 'returns GroSocial::User instances' do
+      # GroSocial::Users.each do |user|
+      #   expect(user).to be_a_kind_of(GroSocial::User)
+      # end
+    end
   end
 end
